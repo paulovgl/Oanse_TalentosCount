b863128d89f60ac9ab3caca9b5938e04cdc515d9
paulovgl <ditoprimo@gmail.com>
2023-01-06 18:47:06 -0300

    accordion animation done

--- js/index.js
+++ js/index.js
@@ -2,9 +2,14 @@ var acc = document.querySelector(`[data-js="accordion"]`);
 
 acc.addEventListener("click", (e) => {
   const accordionHeaderId = e.target.dataset.accordionHeader;
+  const clickedAccordionHeader = document.querySelector(
+    `[data-accordion-header="${accordionHeaderId}"]`
+  );
   const accordionToBeOpened = document.querySelector(
     `[data-accordion-body="${accordionHeaderId}"]`
   );
-  
+
+  clickedAccordionHeader.classList.toggle("actived");
   accordionToBeOpened.classList.toggle("actived");
+
 });
