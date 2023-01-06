a09542e64eb6c02d27db963a471a78fe5b0bc2e7
paulovgl <ditoprimo@gmail.com>
2023-01-06 19:14:53 -0300

    funcionalidade: fechar accordion anterior

--- js/index.js
+++ js/index.js
@@ -8,8 +8,20 @@ acc.addEventListener("click", (e) => {
   const accordionToBeOpened = document.querySelector(
     `[data-accordion-body="${accordionHeaderId}"]`
   );
+  const accordionToBeClosed = Array.from(
+    document.querySelectorAll(`[data-js="accordion-header"]`)
+  )
+    .filter((accordionHeader) => accordionHeader !== clickedAccordionHeader)
+    .find((accordionHeader) => accordionHeader.classList.contains("actived"));
+
+  if (accordionToBeClosed) {
+    const accordionHeaderId = accordionToBeClosed.dataset.accordionHeader;
+    const accordionBodyToBeClosed = document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`);
+
+    accordionToBeClosed.classList.remove("actived");
+    accordionBodyToBeClosed.classList.remove("actived");
+  }
 
   clickedAccordionHeader.classList.toggle("actived");
   accordionToBeOpened.classList.toggle("actived");
-
 });
