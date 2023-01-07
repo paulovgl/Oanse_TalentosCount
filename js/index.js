03d26abd3088f59eaa65d1e4dd1023278cac5c6c
paulovgl <ditoprimo@gmail.com>
2023-01-07 13:52:24 -0300

    added graus testes to accordion body

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const acc = document.querySelector(`[data-js="accordion"]`);
 const caminhante = document.getElementsByName("caminhante");
 const saltitante = document.getElementsByName("saltitante");
+const escalador = document.getElementsByName("escalador");
+const sabia = document.getElementsByName("sabia");
+const aguia = document.getElementsByName("aguia");
+const carneiro = document.getElementsByName("carneiro");
+const leao = document.getElementsByName("leao");
+const sprint = document.getElementsByName("sprint");
+const maratona = document.getElementsByName("maratona");
 const resultOutput = document.querySelector(".result-output");
 const resultBtn = document.querySelector(".calcular");
 
@@ -65,20 +72,61 @@ const talentosCalculator = () => {
       }
       break;
     case 3:
+      for (let i = 0; i < escalador.length; i++) {
+        if (escalador[i].checked) {
+          talentos.push(parseInt(escalador[i].value));
+        }
+        clearChecked(escalador[i]);
+      }
       break;
     case 4:
+      for (let i = 0; i < sabia.length; i++) {
+        if (sabia[i].checked) {
+          talentos.push(parseInt(sabia[i].value));
+        }
+        clearChecked(sabia[i]);
+      }
       break;
     case 5:
+      for (let i = 0; i < aguia.length; i++) {
+        if (aguia[i].checked) {
+          talentos.push(parseInt(aguia[i].value));
+        }
+        clearChecked(aguia[i]);
+      }
       break;
     case 6:
+      for (let i = 0; i < carneiro.length; i++) {
+        if (carneiro[i].checked) {
+          talentos.push(parseInt(carneiro[i].value));
+        }
+        clearChecked(carneiro[i]);
+      }
       break;
     case 7:
+      for (let i = 0; i < leao.length; i++) {
+        if (leao[i].checked) {
+          talentos.push(parseInt(leao[i].value));
+        }
+        clearChecked(leao[i]);
+      }
       break;
     case 8:
+      for (let i = 0; i < sprint.length; i++) {
+        if (sprint[i].checked) {
+          talentos.push(parseInt(sprint[i].value));
+        }
+        clearChecked(sprint[i]);
+      }
       break;
     case 9:
+      for (let i = 0; i < maratona.length; i++) {
+        if (maratona[i].checked) {
+          talentos.push(parseInt(maratona[i].value));
+        }
+        clearChecked(maratona[i]);
+      }
       break;
-
     default:
       break;
   }
